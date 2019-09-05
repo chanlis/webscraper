822faa05db07f2785c287dd50767b7803ba4963e
Lisa Chan <lisachan.nb@gmail.com>
2019-09-05 16:39:04 -0400

    remove state and css selectors

--- edgar/edgar/spiders/scraper.py
+++ edgar/edgar/spiders/scraper.py
@@ -11,64 +11,46 @@ from urllib.parse import urlencode
 DOMAIN = 'https://www.sec.gov'
 EDGAR_URL = DOMAIN + '/cgi-bin/browse-edgar'
 
-RESULTS = 'results'
-DOCUMENTS = 'documents'
-REPORT = 'report'
-
 class EdgarSpider(scrapy.Spider):
     name = 'edgar'
     def __init__(self, cik='0001166559', *args, **kwargs):
-        super(Scraper, self).__init__(*args, **kwargs)
+        super(EdgarSpider, self).__init__(*args, **kwargs)
         query_str = urlencode({
             'CIK': cik,
             'action': 'getcompany',
             'owner': 'exclude',
             'type': '13F-HR'
         }, doseq=True)
+
+        self.cik = cik
         self.start_urls = [EDGAR_URL + '?' + query_str]
-        self.state = RESULTS
 
     def parse(self, response):
-        if self.state == RESULTS:
-            yield from self.parse_results(response)
-        elif self.state == DOCUMENTS:
-            yield from self.parse_documents(response)
-        elif self.state == REPORT:
-            yield from self.parse_report(response)
-        else:
-            pass
-            # TODO: THROW EXCEPTION(INVALID STATE)
-
-    def parse_results(self, response):
-        document_path = response.css('table.tableFile2 a::attr(href)').get()
+        document_path = response.xpath('//table[@class=\'tableFile2\']//a/@href').get()
         if document_path:
-            self.state = DOCUMENTS
-            yield response.follow(DOMAIN + document_path, self.parse)
+            yield response.follow(DOMAIN + document_path, self.parse_documents)
         else:
-            print('No holdings found.')
+            raise Exception('No holdings found from', response.url)
 
     def parse_documents(self, response):
-        table = response.css('table.tableFile tr')
-        if not table:
-            # TODO: INVALID, TABLE NOT FOUND
-            return
-
-        table_header = table[0].xpath('th//text()').getall()
+        table = response.xpath('//table[@class=\'tableFile\']//tr')
+        table_header = table[0].xpath('//th//text()').getall()
         col_index = {}
+
         for index, header in enumerate(table_header):
-            if header.lower() in ['document', 'type']:
-                col_index[header.lower()] = index
+            header = header.lower()
+            if header in ['document', 'type']:
+                col_index[header] = index
 
         for row in table:
-            row_data = row.css('td')
+            row_data = row.xpath('td')
             if row_data:
                 type = row_data[col_index['type']].xpath('text()').get()
-                document = row_data[col_index['document']].css('a')
+                document = row_data[col_index['document']].xpath('./a')
                 document_text = document.xpath('text()').get() if document else ''
                 if type.lower() == 'information table' and 'xml' in document_text:
-                    self.state = REPORT
                     report_path = document.xpath('@href').get()
-                    yield response.follow(DOMAIN + report_path, self.parse)
+                    yield response.follow(DOMAIN + report_path, self.parse_report)
 
     def parse_report(self, response):
         report = Selector(text=response.text)
